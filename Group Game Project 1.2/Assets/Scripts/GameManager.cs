2329e26fdb0559057265fe50438de5536d418383
Kahalo3259 <Rensojunior1@hotmail.com>
2017-10-11 00:32:44 -0400

    Added the ControllerSync Menu
    Added all menus up to ControllerSync, just need to link the player class to the index of the respective controller so that when you play a mini game, the player can be linked to a controllable game object that will keep score for that player, once we figure that out, is a simple matter of starting the game, loading a random minigame and doing that until a game over. Note: add minigame scenes into an array of scenes. Everything in this commit has been tested by me and worked.

---
+++ Group Game Project 1.2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static Player[] players;
+    static bool gameStarted = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		while (gameStarted)
+        {
+
+        }
+	}
+
+    public static void setPlayers(int amountOfPlayers)
+    {
+        players = new Player[amountOfPlayers];
+        ControllerSyncMenu.spawnTesters();
+    }
+
+    public static void startGame()
+    {
+        gameStarted = true;
+    }
+}
